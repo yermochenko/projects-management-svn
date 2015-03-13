b6ffc3519a222353ee493e2871fa73a7a33b2768
yermochenko <yermochenko@gmail.com>
2015-03-13 06:19:48 +0000

    Rename class ContactType to ContactsType for better understanding

--- src/by/vsu/mf/ammc/pm/domain/user/ContactsType.java
+++ src/by/vsu/mf/ammc/pm/domain/user/ContactsType.java
@@ -2,7 +2,7 @@ package by.vsu.mf.ammc.pm.domain.user;
 
 import by.vsu.mf.ammc.pm.domain.NamedEntity;
 
-public class ContactType extends NamedEntity {
+public class ContactsType extends NamedEntity {
 	private String regexp;
 
 	public String getRegexp() {
