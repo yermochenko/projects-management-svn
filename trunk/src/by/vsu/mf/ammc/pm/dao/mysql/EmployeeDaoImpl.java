9ff625e1a755eb3fe53214fd1c8121c844afe01d
meskillOk@gmail.com <meskillOk@gmail.com@7040c96a-f151-7552-e6c8-440674ab3d09>
2015-03-19 18:46:45 +0000

    Created EmployeeDaoImpl class Created EmployeeDaoTest class

---
+++ trunk/src/by/vsu/mf/ammc/pm/dao/mysql/EmployeeDaoImpl.java
@@ -0,0 +1,124 @@
+package by.vsu.mf.ammc.pm.dao.mysql;
+
+import by.vsu.mf.ammc.pm.dao.abstraction.project.management.EmployeeDao;
+import by.vsu.mf.ammc.pm.domain.project.management.Employee;
+import by.vsu.mf.ammc.pm.domain.project.management.EmployeesRole;
+import by.vsu.mf.ammc.pm.domain.project.management.Team;
+import by.vsu.mf.ammc.pm.domain.user.User;
+import by.vsu.mf.ammc.pm.exception.PersistentException;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by meskill on 19.3.15.
+ */
+public class EmployeeDaoImpl extends BaseDaoImpl implements EmployeeDao {
+    @Override
+    public Integer create(Employee entity) throws PersistentException {
+        String sql = "INSERT INTO `employee` (`user_id`,`team_id`,`role`) VALUES (?,?,?)";
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, entity.getUser().getId());
+            statement.setInt(2, entity.getTeam().getId());
+            statement.setByte(3, (byte) entity.getRole().ordinal());
+            statement.executeUpdate();
+            resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            } else {
+                throw new PersistentException();
+            }
+        } catch (SQLException e) {
+            throw new PersistentException(e);
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException | NullPointerException e) {
+            }
+            try {
+                statement.close();
+            } catch (SQLException | NullPointerException e) {
+            }
+        }
+    }
+
+    @Override
+    public Employee read(Integer id) throws PersistentException {
+        String sql = "SELECT `id`,`user_id`,`team_id`,`role` FROM `employee` WHERE `id`=?";
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = this.getConnection().prepareStatement(sql);
+            statement.setInt(1, id);
+            resultSet = statement.executeQuery();
+            Employee employee = null;
+            if (resultSet.next()) {
+                employee = new Employee();
+                employee.setId(id);
+                User user = new User();
+                user.setId(resultSet.getInt("user_id"));
+                employee.setUser(user);
+                Team team = new Team();
+                team.setId(resultSet.getInt("team_id"));
+                employee.setTeam(team);
+                employee.setRole(EmployeesRole.values()[resultSet.getByte("role")]);
+            }
+            return employee;
+        } catch (SQLException e) {
+            throw new PersistentException(e);
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException | NullPointerException e) {
+            }
+            try {
+                statement.close();
+            } catch (SQLException | NullPointerException e) {
+            }
+        }
+    }
+
+    @Override
+    public void update(Employee entity) throws PersistentException {
+        String sql = "UPDATE `employee` SET `user_id`= ?, `team_id`=?, `role`=? WHERE `id` = ?";
+        PreparedStatement statement = null;
+        try {
+            statement = getConnection().prepareStatement(sql);
+            statement.setInt(1, entity.getUser().getId());
+            statement.setInt(2, entity.getTeam().getId());
+            statement.setByte(3, (byte) entity.getRole().ordinal());
+            statement.setInt(4, entity.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new PersistentException(e);
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException | NullPointerException e) {
+            }
+        }
+    }
+
+    @Override
+    public void delete(Integer id) throws PersistentException {
+        String sql = "DELETE FROM `employee` WHERE `id` = ?";
+        PreparedStatement statement = null;
+        try {
+            statement = getConnection().prepareStatement(sql);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new PersistentException(e);
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException | NullPointerException e) {
+            }
+        }
+    }
+}
---
+++ trunk/src/test/project/management/EmployeeDaoTest.java
@@ -0,0 +1,65 @@
+package test.project.management;
+
+import by.vsu.mf.ammc.pm.dao.mysql.EmployeeDaoImpl;
+import by.vsu.mf.ammc.pm.dao.util.pool.ConnectionPool;
+import by.vsu.mf.ammc.pm.domain.project.management.Employee;
+import by.vsu.mf.ammc.pm.domain.project.management.EmployeesRole;
+import by.vsu.mf.ammc.pm.domain.project.management.Team;
+import by.vsu.mf.ammc.pm.domain.user.User;
+import by.vsu.mf.ammc.pm.exception.PersistentException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by meskill on 19.3.15.
+ */
+public class EmployeeDaoTest {
+
+    public static void main(String[] args) throws PersistentException, SQLException {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        pool.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost", "root", "root", 1, 1, 10000);
+        EmployeeDaoImpl dao = new EmployeeDaoImpl();
+        Connection conn = pool.getConnection();
+        conn.createStatement().execute("use pm_db");
+
+
+        dao.setConnection(conn);
+        // create
+        Employee create_employee = new Employee();
+        int create_id = -1;
+        User user = new User();
+        user.setId(13001);
+        Team team = new Team();
+        team.setId(13001);
+        create_employee.setUser(user);
+        create_employee.setTeam(team);
+        create_employee.setRole(EmployeesRole.BUSINESS_ANALYST);
+        create_id = dao.create(create_employee);
+        assert create_id != -1 : "Failed on create";
+
+        //read
+        Employee read_employee = dao.read(create_id);
+        assert create_employee.getUser().getId() == read_employee.getUser().getId() &&
+                create_employee.getTeam().getId() == read_employee.getTeam().getId() &&
+                create_employee.getRole() == read_employee.getRole() : "Failed or read";
+
+        //update
+        Employee update_employee = new Employee();
+        user.setId(13002);
+        team.setId(13002);
+        update_employee.setId(create_id);
+        update_employee.setUser(user);
+        update_employee.setTeam(team);
+        update_employee.setRole(EmployeesRole.PROJECT_MANAGER);
+        dao.update(update_employee);
+        read_employee = dao.read(create_id);
+        assert update_employee.getUser().getId() == read_employee.getUser().getId() &&
+                update_employee.getTeam().getId() == read_employee.getTeam().getId() &&
+                update_employee.getRole() == read_employee.getRole() : "Failed on update";
+
+        //delete
+        dao.delete(create_id);
+        assert dao.read(create_id) == null : "Failed on delete";
+    }
+}
