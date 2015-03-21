584dbad4e92f8b03e514b6533247bb7bd8bf9c86
yermochenko <yermochenko@gmail.com>
2015-03-21 10:00:09 +0000

    Fix code style

--- src/by/vsu/mf/ammc/pm/dao/mysql/project/management/TeamDaoImpl.java
+++ src/by/vsu/mf/ammc/pm/dao/mysql/project/management/TeamDaoImpl.java
@@ -1,4 +1,5 @@
 package by.vsu.mf.ammc.pm.dao.mysql.project.management;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,7 +11,7 @@ import by.vsu.mf.ammc.pm.domain.project.management.Team;
 import by.vsu.mf.ammc.pm.domain.user.User;
 import by.vsu.mf.ammc.pm.exception.PersistentException;
 
-public class TeamDaoImpl extends BaseDaoImpl implements TeamDao{	
+public class TeamDaoImpl extends BaseDaoImpl implements TeamDao {
 	@Override
 	public Integer create(Team entity) throws PersistentException {
 		String sql = "INSERT INTO `team` (`project_id`, `leader_id`) VALUES (?, ?)";
@@ -19,12 +20,12 @@ public class TeamDaoImpl extends BaseDaoImpl implements TeamDao{
 		try {
 			statement = getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 			statement.setInt(1, entity.getProject().getId());
-			statement.setInt(2, entity.getLeader().getId());			
+			statement.setInt(2, entity.getLeader().getId());
 			statement.executeUpdate();
 			resultSet = statement.getGeneratedKeys();
 			if(resultSet.next()) {
 				return resultSet.getInt(1);
-			} else {				
+			} else {
 				throw new PersistentException();
 			}
 		} catch(SQLException e) {
@@ -52,12 +53,12 @@ public class TeamDaoImpl extends BaseDaoImpl implements TeamDao{
 			if(resultSet.next()) {
 				team = new Team();
 				team.setId(id);
-                Project project = new Project();
-                project.setId(resultSet.getInt("id"));
-                team.setProject(project);
-                User leader = new User();
-                leader.setId(resultSet.getInt("id"));
-                team.setLeader(leader);
+				Project project = new Project();
+				project.setId(resultSet.getInt("id"));
+				team.setProject(project);
+				User leader = new User();
+				leader.setId(resultSet.getInt("id"));
+				team.setLeader(leader);
 			}
 			return team;
 		} catch(SQLException e) {
@@ -69,7 +70,7 @@ public class TeamDaoImpl extends BaseDaoImpl implements TeamDao{
 			try {
 				statement.close();
 			} catch(SQLException | NullPointerException e) {}
-		} 
+		}
 	}
 
 	@Override
@@ -89,7 +90,7 @@ public class TeamDaoImpl extends BaseDaoImpl implements TeamDao{
 				statement.close();
 			} catch(SQLException | NullPointerException e) {}
 		}
-		
+
 	}
 
 	@Override
@@ -107,7 +108,7 @@ public class TeamDaoImpl extends BaseDaoImpl implements TeamDao{
 				statement.close();
 			} catch(SQLException | NullPointerException e) {}
 		}
-		
+
 	}
 
 }
