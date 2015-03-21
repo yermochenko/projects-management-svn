481f4a75ce5e06b983bca5aff7131ba5f98a8bf9
yermochenko <yermochenko@gmail.com>
2015-03-21 12:29:31 +0000

    Update issue 6 Status: Approved Fix problems with reqexps and NULL identities: 1. Data in table `contact` didn't correspond to regexps intable `contacts_type` 2. One and only one field in table "requirement" ("use_case_id" or "module_id") has to be not empty. 3. One and only one field in table "task" ("requirement_id" or "module_id") has to be not empty

--- sql/11_fill_tables.sql
+++ sql/11_fill_tables.sql
@@ -14,13 +14,13 @@ VALUES (11004,"StanislavAG","202cb962ac59075b964b07152d234b70","Станисла
 
 
 INSERT INTO  `contacts_type` (`id`,`name`,`regexp`) 
-VALUES (11000,"Contact Type 1","Reg expression");
+VALUES (11000,"Contact Type 1",".*");
 INSERT INTO  `contacts_type` (`id`,`name`,`regexp`) 
-VALUES (11001,"Contact Type 2","Reg expression");
+VALUES (11001,"Contact Type 2",".*");
 INSERT INTO  `contacts_type` (`id`,`name`,`regexp`) 
-VALUES (11002,"Contact Type 3","Reg expression");
+VALUES (11002,"Contact Type 3",".*");
 INSERT INTO  `contacts_type` (`id`,`name`,`regexp`) 
-VALUES (11003,"Contact Type 4","Reg expression");
+VALUES (11003,"Contact Type 4",".*");
 
 INSERT INTO  `contact` (`id`,`name`,`user_id`,`type_id`) 
 VALUES (11000,"Contact 1",11000,11000);
@@ -42,7 +42,7 @@ VALUES (11001,"Projects category 1.1",11000);
 INSERT INTO  `projects_category` (`id`,`name`,`parent_id`) 
 VALUES (11002,"Projects category 2",NULL);
 INSERT INTO  `projects_category` (`id`,`name`,`parent_id`) 
-VALUES (11003,"Projects category 1.1.1",NULL);
+VALUES (11003,"Projects category 1.1.1",11001);
 
 INSERT INTO  `project` (`id`,`name`,`description`,`category_id`,`manager_id`) 
 VALUES (11000,"Project 1","Some project 1 description",11000,11000);
@@ -134,23 +134,23 @@ INSERT INTO  `actor_use_case_relation` (`id`,`actor_id`,`use_case_id`)
 VALUES (11008,11002,11006);
 
 INSERT INTO  `requirement` (`id`,`name`,`description`,`importance`,`change_probability`,`use_case_id`,`module_id`) 
-VALUES (11000,"Requirement 1","Some requirement description",1.0,0.0,11000,11000);
+VALUES (11000,"Requirement 1","Some requirement description",1.0,0.0,11000,NULL);
 INSERT INTO  `requirement` (`id`,`name`,`description`,`importance`,`change_probability`,`use_case_id`,`module_id`) 
-VALUES (11001,"Requirement 2","Some requirement description",0.5,0.0,11001,11001);
+VALUES (11001,"Requirement 2","Some requirement description",0.5,0.0,11001,NULL);
 INSERT INTO  `requirement` (`id`,`name`,`description`,`importance`,`change_probability`,`use_case_id`,`module_id`) 
-VALUES (11002,"Requirement 3","Some requirement description",1.0,0.0,11002,11002);
+VALUES (11002,"Requirement 3","Some requirement description",1.0,0.0,11002,NULL);
 INSERT INTO  `requirement` (`id`,`name`,`description`,`importance`,`change_probability`,`use_case_id`,`module_id`) 
-VALUES (11003,"Requirement 4","Some requirement description",0.5,0.0,11003,11003);
+VALUES (11003,"Requirement 4","Some requirement description",0.5,0.0,NULL,11003);
 INSERT INTO  `requirement` (`id`,`name`,`description`,`importance`,`change_probability`,`use_case_id`,`module_id`) 
-VALUES (11004,"Requirement 5","Some requirement description",0.5,0.7,11004,11004);
+VALUES (11004,"Requirement 5","Some requirement description",0.5,0.7,11004,NULL);
 INSERT INTO  `requirement` (`id`,`name`,`description`,`importance`,`change_probability`,`use_case_id`,`module_id`) 
-VALUES (11005,"Requirement 6","Some requirement description",0.5,0.0,11005,11005);
+VALUES (11005,"Requirement 6","Some requirement description",0.5,0.0,11005,NULL);
 INSERT INTO  `requirement` (`id`,`name`,`description`,`importance`,`change_probability`,`use_case_id`,`module_id`) 
-VALUES (11006,"Requirement 7","Some requirement description",0.5,0.0,11006,11006);
+VALUES (11006,"Requirement 7","Some requirement description",0.5,0.0,11006,NULL);
 INSERT INTO  `requirement` (`id`,`name`,`description`,`importance`,`change_probability`,`use_case_id`,`module_id`) 
-VALUES (11007,"Requirement 8","Some requirement description",0.5,0.0,NULL,NULL);
+VALUES (11007,"Requirement 8","Some requirement description",0.5,0.0,NULL,11004);
 INSERT INTO  `requirement` (`id`,`name`,`description`,`importance`,`change_probability`,`use_case_id`,`module_id`) 
-VALUES (11008,"Requirement 9","Some requirement description",0.5,0.9,NULL,NULL);
+VALUES (11008,"Requirement 9","Some requirement description",0.5,0.9,NULL,11004);
 
 INSERT INTO  `tasks_category` (`id`,`name`,`parent_id`) 
 VALUES (11000,"Task category 1",NULL);
@@ -166,23 +166,23 @@ INSERT INTO  `tasks_category` (`id`,`name`,`parent_id`)
 VALUES (11005,"Task category 2.2",11003);
 
 INSERT INTO  `task` (`id`,`name`,`description`,`plan_time`,`difficulty`,`open_date`,`accept_date`,`close_date`,`category_id`,`requirement_id`,`module_id`,`employee_id`,`status`) 
-VALUES (11000,"Task 1","Some task description",280,0.8,"2015-02-23","2015-02-23","2015-02-26",11000,11000,11000,11001,4);
+VALUES (11000,"Task 1","Some task description",280,0.8,"2015-02-23","2015-02-23" ,"2015-02-26",11000,11000,NULL,11001,4);
 INSERT INTO  `task` (`id`,`name`,`description`,`plan_time`,`difficulty`,`open_date`,`accept_date`,`close_date`,`category_id`,`requirement_id`,`module_id`,`employee_id`,`status`) 
-VALUES (11001,"Task 2","Some task description",180,0.6,"2015-02-23","2015-02-23","2015-03-02",11000,11001,11001,11002,4);
+VALUES (11001,"Task 2","Some task description",180,0.6,"2015-02-23","2015-02-23" ,"2015-03-02",11000,11001,NULL,11002,4);
 INSERT INTO  `task` (`id`,`name`,`description`,`plan_time`,`difficulty`,`open_date`,`accept_date`,`close_date`,`category_id`,`requirement_id`,`module_id`,`employee_id`,`status`) 
-VALUES (11002,"Task 3","Some task description",170,0.6,"2015-02-23","2015-02-26","2015-03-03",11000,11002,11002,11003,4);
+VALUES (11002,"Task 3","Some task description",170,0.6,"2015-02-23","2015-04-26" ,"2015-05-03",11000,11002,NULL,11003,4);
 INSERT INTO  `task` (`id`,`name`,`description`,`plan_time`,`difficulty`,`open_date`,`accept_date`,`close_date`,`category_id`,`requirement_id`,`module_id`,`employee_id`,`status`) 
-VALUES (11003,"Task 4","Some task description",180,0.6,"2015-02-23","2015-02-23","2015-03-03",11000,11003,11003,11004,4);
+VALUES (11003,"Task 4","Some task description",180,0.6,"2015-02-23","2015-02-23" ,"2015-03-03",11000,11003,NULL,11004,4);
 INSERT INTO  `task` (`id`,`name`,`description`,`plan_time`,`difficulty`,`open_date`,`accept_date`,`close_date`,`category_id`,`requirement_id`,`module_id`,`employee_id`,`status`) 
-VALUES (11004,"Task 5","Some task description",250,0.5,"2015-02-23","2015-02-26","2015-03-03",11001,11004,11004,11001,4);
+VALUES (11004,"Task 5","Some task description",250,0.5,"2015-02-23","2015-02-26" ,"2015-03-03",11001,11004,NULL,11001,4);
 INSERT INTO  `task` (`id`,`name`,`description`,`plan_time`,`difficulty`,`open_date`,`accept_date`,`close_date`,`category_id`,`requirement_id`,`module_id`,`employee_id`,`status`) 
-VALUES (11005,"Task 6","Some task description",160,0.4,"2015-02-23","2015-03-02","2015-03-05",11001,11005,11005,11002,4);
+VALUES (11005,"Task 6","Some task description",160,0.4,"2015-02-23","2015-03-02" ,"2015-03-05",11001,11005,NULL,11002,4);
 INSERT INTO  `task` (`id`,`name`,`description`,`plan_time`,`difficulty`,`open_date`,`accept_date`,`close_date`,`category_id`,`requirement_id`,`module_id`,`employee_id`,`status`) 
-VALUES (11006,"Task 7","Some task description",380,0.9,"2015-02-23","2015-03-03","2015-03-05",11001,11006,11006,11003,4);
+VALUES (11006,"Task 7","Some task description",380,0.9,"2015-02-23","2015-03-03" ,"2015-03-05",11001,11006,NULL,11003,4);
 INSERT INTO  `task` (`id`,`name`,`description`,`plan_time`,`difficulty`,`open_date`,`accept_date`,`close_date`,`category_id`,`requirement_id`,`module_id`,`employee_id`,`status`) 
-VALUES (11007,"Task 8","Some task description",280,0.8,"2015-02-26",NULL,NULL,11002,11007,11007,11004,0);
+VALUES (11007,"Task 8","Some task description",280,0.8,"2015-02-26",NULL,NULL,11002,NULL,11007,NULL,0);
 INSERT INTO  `task` (`id`,`name`,`description`,`plan_time`,`difficulty`,`open_date`,`accept_date`,`close_date`,`category_id`,`requirement_id`,`module_id`,`employee_id`,`status`) 
-VALUES (11008,"Task 9","Some task description",100,0.3,"2015-02-26",NULL,NULL,11002,11008,11008,11004,0);
+VALUES (11008,"Task 9","Some task description",100,0.3,"2015-02-26",NULL,NULL,11002,NULL,11008,NULL,0);
 
 INSERT INTO  `tasks_dependency` (`id`,`task_id`,`dependent_task_id`) 
 VALUES (11000,11000,11002);
