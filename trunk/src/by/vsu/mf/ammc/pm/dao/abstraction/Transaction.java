32de34ee0dc3f3684c1ba555b27a50015b11f66d
yermochenko@gmail.com <yermochenko@gmail.com@7040c96a-f151-7552-e6c8-440674ab3d09>
2015-03-26 21:28:30 +0000

    DAO factory implemented

---
+++ trunk/src/by/vsu/mf/ammc/pm/dao/abstraction/Transaction.java
@@ -0,0 +1,13 @@
+package by.vsu.mf.ammc.pm.dao.abstraction;
+
+import by.vsu.mf.ammc.pm.exception.PersistentException;
+
+public interface Transaction {
+	<Type extends Dao<?>> Type getDao(Class<Type> key) throws PersistentException;
+
+	void commit() throws PersistentException;
+
+	void rollback() throws PersistentException;
+
+	void close();
+}
