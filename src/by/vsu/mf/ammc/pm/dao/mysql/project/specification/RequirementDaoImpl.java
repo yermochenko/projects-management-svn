47e50356b5f00c0802d55d105d51f64fd33ac913
Bulachnik <bulachnik@gmail.com>
2015-03-28 09:32:59 +0000

    Update issue 25 Status: Done Create class RequirementDaoImpl which implements interface RequirementDao. Create test class RequirementDaoTest which tests each method of class RequirementDaoImpl. Test class is to contain method main and have simple console implementation.

---
+++ src/by/vsu/mf/ammc/pm/dao/mysql/project/specification/RequirementDaoImpl.java
@@ -0,0 +1,155 @@
+package by.vsu.mf.ammc.pm.dao.mysql.project.specification;
+
+import by.vsu.mf.ammc.pm.dao.abstraction.project.specification.RequirementDao;
+import by.vsu.mf.ammc.pm.dao.mysql.BaseDaoImpl;
+import by.vsu.mf.ammc.pm.domain.project.Module;
+import by.vsu.mf.ammc.pm.domain.project.specification.Requirement;
+import by.vsu.mf.ammc.pm.domain.project.specification.UseCase;
+import by.vsu.mf.ammc.pm.exception.PersistentException;
+import org.apache.log4j.Logger;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+
+public class RequirementDaoImpl extends BaseDaoImpl implements RequirementDao  {
+    private static Logger logger = Logger.getLogger( RequirementDaoImpl.class);
+    @Override
+    public Integer create(Requirement entity) throws PersistentException {
+        String sql = " INSERT INTO requirement ( name, description, importance, change_probability, use_case_id, module_id ) VALUES ( ?, ?, ?, ?, ?, ? ); ";
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = getConnection().prepareStatement( sql, preparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString( 1, entity.getName() );
+            preparedStatement.setString( 2, entity.getDescription() );
+            preparedStatement.setFloat(3, entity.getImportance());
+            preparedStatement.setFloat(4, entity.getChangeProbability());
+            if (  entity.getUseCase() != null  &&  entity.getUseCase().getId() != null ) {
+                preparedStatement.setInt( 5, entity.getUseCase().getId() );
+            } else {
+                preparedStatement.setNull( 5, Types.INTEGER);
+            }
+            if ( entity.getModule() != null && entity.getModule().getId() != null ) {
+                preparedStatement.setInt( 6, entity.getModule().getId() );
+            } else {
+                preparedStatement.setNull( 6, Types.INTEGER );
+            }
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+            if( resultSet.next() ) {
+                return resultSet.getInt( 1 );
+            } else {
+                logger.error( "There is problem with index after trying to add record into table 'requirement'" );
+                throw new PersistentException();
+            }
+        } catch ( SQLException e ){
+            logger.error( "Creation of record was failed. Table 'requirement'", e );
+            throw new PersistentException( e );
+        } finally {
+            try {
+                resultSet.close();
+            } catch ( SQLException | NullPointerException e){}
+            try {
+                preparedStatement.close();
+            } catch ( SQLException | NullPointerException e){}
+        }
+    }
+
+    @Override
+    public Requirement read(Integer id) throws PersistentException {
+        String sql = " SELECT name, description, importance, change_probability, use_case_id, module_id FROM requirement WHERE id = ? ";
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = getConnection().prepareStatement( sql, preparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt( 1 , id );
+            resultSet = preparedStatement.executeQuery();
+            Requirement requirement = new Requirement();
+            if( resultSet.next() ) {
+                requirement.setId( id );
+                requirement.setName( resultSet.getString("name") );
+                requirement.setDescription( resultSet.getString( "description") );
+                requirement.setImportance( resultSet.getFloat( "importance" ) );
+                requirement.setChangeProbability( resultSet.getFloat( "change_probability" ) );
+                Integer useCaseId = resultSet.getInt( "use_case_id" );
+                if( !resultSet.wasNull() ) {
+                    UseCase useCase = new UseCase();
+                    useCase.setId( useCaseId );
+                    requirement.setUseCase( useCase );
+                }
+                Integer moduleId = resultSet.getInt( "module_id" );
+                if( !resultSet.wasNull() ) {
+                    Module module = new Module();
+                    module.setId(moduleId);
+                    requirement.setModule(module);
+                }
+            }
+            return requirement;
+        } catch( SQLException e) {
+            logger.error( "Reading of record was failed. Table 'requirement'", e);
+            throw new PersistentException(e);
+        } finally {
+            try {
+                resultSet.close();
+            } catch ( SQLException | NullPointerException e) {}
+            try {
+                preparedStatement.close();
+            } catch ( SQLException | NullPointerException e) {}
+        }
+    }
+
+    @Override
+    public void update(Requirement entity) throws PersistentException {
+        String sql = " UPDATE requirement SET name = ?, description = ?, importance = ?, change_probability = ?, use_case_id = ?, module_id = ? WHERE id = ?";
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = getConnection().prepareStatement( sql );
+
+            preparedStatement.setString(1, entity.getName());
+            preparedStatement.setString( 2, entity.getDescription() );
+            preparedStatement.setFloat( 3 , entity.getImportance() );
+            preparedStatement.setFloat( 4, entity.getChangeProbability() );
+            if( entity.getUseCase() != null && entity.getUseCase().getId() != null ) {
+                preparedStatement.setInt( 5 , entity.getUseCase().getId());
+            } else  {
+                preparedStatement.setNull( 5 , Types.INTEGER );
+            }
+            if( entity.getModule() != null && entity.getModule().getId() != null ) {
+                preparedStatement.setInt( 6 , entity.getModule().getId());
+            } else  {
+                preparedStatement.setNull( 6 , Types.INTEGER );
+            }
+            preparedStatement.setInt( 7, entity.getId() );
+            preparedStatement.executeUpdate();
+        } catch( SQLException e) {
+            logger.error( "Updating of record was failed. Table 'requirement'", e);
+            throw new PersistentException(e);
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch ( SQLException | NullPointerException e) {}
+        }
+    }
+
+    @Override
+    public void delete(Integer id) throws PersistentException {
+        String sql = " DELETE FROM requirement WHERE id = ?";
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = getConnection().prepareStatement( sql );
+            preparedStatement.setInt( 1, id );
+            preparedStatement.executeUpdate();
+
+        }catch ( SQLException e) {
+            logger.error( "Deleting of record was failed. Table 'requirement' ", e);
+            throw new PersistentException(e);
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch ( SQLException  | NullPointerException e) {}
+        }
+    }
+}
---
+++ src/by/vsu/mf/ammc/pm/test/project/specification/RequirementDaoImplTest.java
@@ -0,0 +1,81 @@
+package by.vsu.mf.ammc.pm.test.project.specification;
+
+import by.vsu.mf.ammc.pm.dao.mysql.project.specification.RequirementDaoImpl;
+import by.vsu.mf.ammc.pm.dao.util.pool.ConnectionPool;
+import by.vsu.mf.ammc.pm.domain.project.specification.Requirement;
+import by.vsu.mf.ammc.pm.exception.PersistentException;
+
+import java.sql.SQLException;
+
+public class RequirementDaoImplTest {
+    public static void main ( String [] args) throws PersistentException, SQLException {
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        try {
+            connectionPool.init( "com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306", "root", "root", 1, 20, 10000 );
+        } catch (PersistentException e) {
+            e.printStackTrace();
+        }
+        System.out.println( "TestCreate" );
+        String testPassed = "\ttest passed";
+        String testFailed = "\ttest failed";
+        String testResult = testPassed;
+        RequirementDaoImpl requirementDao = new RequirementDaoImpl();
+        requirementDao.setConnection( connectionPool.getConnection() );
+        requirementDao.getConnection().prepareStatement( "use pm_db" ).executeQuery();
+        Requirement requirement = new Requirement();
+        requirement.setName( "TestRequirement" );
+        requirement.setDescription("It's test requirement");
+        requirement.setImportance( new Float( 1.0 ) );
+        requirement.setChangeProbability( new Float( 0.5 ) );
+        requirement.setUseCase( null );
+        requirement.setModule( null );
+        try {
+            requirementDao.create( requirement );
+        } catch ( PersistentException e) {
+            testResult = testFailed;
+        }
+
+        System.out.println( testResult );
+        testResult = testPassed;
+
+        System.out.println( "TestRead" );
+        try {
+            requirementDao.read(1);
+        } catch ( PersistentException e) {
+            testResult = testFailed;
+        }
+
+        System.out.println( testResult );
+        testResult = testPassed;
+
+        System.out.println( "TestUpdate" );
+        Requirement updatedRequirement = new Requirement();
+        updatedRequirement.setId( 2 );
+        updatedRequirement.setName("UpdateRequirement");
+        updatedRequirement.setDescription("It's updated requirement");
+        updatedRequirement.setImportance(new Float(0.5));
+        updatedRequirement.setChangeProbability(new Float(0.3));
+        updatedRequirement.setUseCase(null);
+        updatedRequirement.setModule(null);
+        try {
+            requirementDao.update( updatedRequirement );
+        } catch (PersistentException e) {
+            testResult = testFailed;
+        }
+
+        System.out.println( testResult );
+        testResult = testPassed;
+
+        System.out.println( "TestDelete" );
+        try {
+            requirementDao.delete(2);
+        } catch ( PersistentException e ) {
+            testResult = testFailed;
+        }
+        System.out.println( testResult );
+
+
+        connectionPool.destroy();
+    }
+}
+
