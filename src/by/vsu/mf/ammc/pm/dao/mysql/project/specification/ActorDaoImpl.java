af3579a38c879829eb1c38496d4b5c0f78ea6ae8
nixumx <nixumx@7040c96a-f151-7552-e6c8-440674ab3d09>
2015-04-03 14:48:22 +0000

    Update issue 38 Status: Done Implemented a caching of objects in the class by.vsu.mf.ammc.pm.dao.mysql.project.specification.ActorDaoImpl while calling method read(int id) twice or more times. Created class by.vsu.mf.ammc.pm.test.project.specification.ActorIdentityMapTest which test this caching.

--- src/by/vsu/mf/ammc/pm/dao/mysql/project/specification/ActorDaoImpl.java
+++ src/by/vsu/mf/ammc/pm/dao/mysql/project/specification/ActorDaoImpl.java
@@ -9,130 +9,122 @@ import by.vsu.mf.ammc.pm.exception.PersistentException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 
 public class ActorDaoImpl extends BaseDaoImpl implements ActorDao {
 
-	@Override
-	public Integer create(Actor entity) throws PersistentException{
-		
-		Statement statement = null;
-		int resultSet;	
-		
-		try
-		{		
+    private static HashMap<Integer, Actor> hash = new HashMap<>();
+
+    @Override
+    public Integer create(Actor entity) throws PersistentException {
+	Statement statement = null;
+	ResultSet resultSet = null;
+	try {
+	    statement = getConnection().createStatement();
+	    String sql = "INSERT INTO actor (`id`, `name`, `project_id`) " + "VALUES (" + Integer.toString(entity.getId()) + ", '"
+		    + entity.getName() + "', " + Integer.toString(entity.getProject().getId()) + ")";
+	    statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+	    resultSet = statement.getGeneratedKeys();
+	    if (resultSet.next()) {
+		return resultSet.getInt(1);
+	    } else {
+		throw new PersistentException();
+	    }
+	} catch (SQLException e) {
+	    throw new PersistentException(e);
+	} finally {
+	    try {
+		resultSet.close();
+	    } catch (SQLException | NullPointerException e) {
+	    }
+	    try {
+		statement.close();
+	    } catch (SQLException | NullPointerException e) {
+	    }
+	}
+    }
+
+    @Override
+    public Actor read(Integer id) throws PersistentException {
+	Actor actor = getHash(id);
+	if (actor == null) {
+	    Statement statement = null;
+	    ResultSet resultSet = null;
+	    try {
 		statement = getConnection().createStatement();
-		String sql = "INSERT INTO actor (`id`, `name`, `project_id`) "
-				 + "VALUES (" + Integer.toString(entity.getId()) + 
-				 ", '" + entity.getName() + "', " + 
-				 Integer.toString(entity.getProject().getId()) + ")";
-		resultSet = statement.executeUpdate(sql);
-		return resultSet;
+		resultSet = statement.executeQuery("SELECT * FROM `actor` WHERE id = " + Integer.toString(id));
+		if (resultSet.next()) {
+		    actor = new Actor();
+		    actor.setId(id);
+		    actor.setName(resultSet.getString("name"));
+		    Project pr = new Project();
+		    pr.setId(resultSet.getInt("project_id"));
+		    actor.setProject(pr);
 		}
-		catch(SQLException e)
-		{
-			throw new PersistentException(e);	
+		setHash(id, actor);
+	    } catch (SQLException e) {
+		throw new PersistentException(e);
+	    } finally {
+		try {
+		    resultSet.close();
+		} catch (SQLException | NullPointerException e) {
 		}
-		finally
-		{
-			try {
-                statement.close();
-			} 
-			catch (SQLException | NullPointerException e) {}			
+		try {
+		    statement.close();
+		} catch (SQLException | NullPointerException e) {
 		}
+	    }
+	}
+	return actor;
+    }
+
+    @Override
+    public void update(Actor entity) throws PersistentException {
+	removeHash(entity.getId());
+	Statement statement = null;
+	try {
+	    statement = getConnection().createStatement();
+	    statement.executeUpdate("UPDATE `actor` SET name = '" + entity.getName() + "', project_id = "
+		    + Integer.toString(entity.getProject().getId()) + " WHERE id = " + entity.getId());
+	} catch (SQLException e) {
+	    throw new PersistentException(e);
+	} finally {
+	    try {
+		statement.close();
+	    } catch (SQLException | NullPointerException e) {
+	    }
 	}
 
-	@Override
-	public Actor read(Integer id) throws PersistentException {
+    }
 
-		Statement statement = null;
-		ResultSet resultSet = null;
-		
-		try
-		{		
-		statement = getConnection().createStatement();
-		resultSet = statement.executeQuery(
-				 "SELECT * FROM `actor` WHERE id = "
-				 + Integer.toString(id) );
-		Actor actor = null;
-		if(resultSet.next())
-		{
-			actor = new Actor();
-			actor.setId(id);
-			actor.setName(resultSet.getString("name"));
-			Project pr = new Project();
-			pr.setId(resultSet.getInt("project_id"));
-			actor.setProject(pr);
-		}
-		return actor;
-		}
-		catch(SQLException e)
-		{
-			throw new PersistentException(e);
-		}
-		finally{
-			try {
-                resultSet.close();
-			} 
-			catch (SQLException | NullPointerException e) {}
-			try {
-                statement.close();
-			} 
-			catch (SQLException | NullPointerException e) {}		
-		}
+    @Override
+    public void delete(Integer id) throws PersistentException {
+	removeHash(id);
+	Statement statement = null;
+	try {
+	    statement = getConnection().createStatement();
+	    statement.executeUpdate("DELETE FROM `actor` WHERE id = " + Integer.toString(id));
+	} catch (SQLException e) {
+	    throw new PersistentException(e);
+	} finally {
+	    try {
+		statement.close();
+	    } catch (SQLException | NullPointerException e) {
+	    }
 	}
 
-	@Override
-	public void update(Actor entity) throws PersistentException {
-		
-		Statement statement = null;
-		
-		try
-		{
-		
-		statement = getConnection().createStatement();
-		statement.executeUpdate(
-				 "UPDATE `actor` SET name = '"
-				 + entity.getName() + "', project_id = " + 
-				 Integer.toString(entity.getProject().getId())
-				 + " WHERE id = " + entity.getId() );	
-		
-		}
-		catch(SQLException e)
-		{
-			throw new PersistentException(e);
-		}
-		finally{
-			try {
-                statement.close();
-			} 
-			catch (SQLException | NullPointerException e) {}		
-		}
+    }
 
-	}
+    private static Actor getHash(Integer id) {
+	return hash.get(id);
+    }
 
-	@Override
-	public void delete(Integer id) throws PersistentException {
+    private static void setHash(Integer id, Actor actor) {
+	ActorDaoImpl.hash.put(id, actor);
+    }
 
-		Statement statement = null;
-		
-		try
-		{		
-		statement = getConnection().createStatement();
-		statement.executeUpdate(
-				 "DELETE FROM `actor` WHERE id = " + 
-				 Integer.toString(id));
-		}
-		catch(SQLException e)
-		{
-			throw new PersistentException(e);
-		}
-		finally{
-			try {
-                statement.close();
-			} 
-			catch (SQLException | NullPointerException e) {}		
-		}
-
-	}
+    private static void removeHash(Integer id) {
+	ActorDaoImpl.hash.remove(id);
+    }
 
 }
