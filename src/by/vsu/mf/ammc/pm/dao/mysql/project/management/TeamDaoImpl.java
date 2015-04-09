5e1aba68a060b9dbb51d11e9b83d1f0f2aad5874
shansiliya <shansiliya@gmail.com>
2015-04-09 11:41:28 +0000

    Update issue 51 Status: Done

--- src/by/vsu/mf/ammc/pm/dao/mysql/project/management/TeamDaoImpl.java
+++ src/by/vsu/mf/ammc/pm/dao/mysql/project/management/TeamDaoImpl.java
@@ -10,87 +10,89 @@ import by.vsu.mf.ammc.pm.dao.abstraction.project.management.TeamDao;
 import by.vsu.mf.ammc.pm.dao.mysql.BaseDaoImpl;
 import by.vsu.mf.ammc.pm.domain.project.Project;
 import by.vsu.mf.ammc.pm.domain.project.management.Team;
-import by.vsu.mf.ammc.pm.domain.project.specification.UseCase;
 import by.vsu.mf.ammc.pm.domain.user.User;
 import by.vsu.mf.ammc.pm.exception.PersistentException;
 
 public class TeamDaoImpl extends BaseDaoImpl implements TeamDao {
-	
+
 	private static HashMap<Integer, Team> hash = new HashMap<>();
-	
+
 	@Override
 	public Integer create(Team entity) throws PersistentException {
 		String sql = "INSERT INTO `team` (`project_id`, `leader_id`) VALUES (?, ?)";
 		PreparedStatement statement = null;
 		ResultSet resultSet = null;
 		try {
-			statement = getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			statement = getConnection().prepareStatement(sql,
+					Statement.RETURN_GENERATED_KEYS);
 			statement.setInt(1, entity.getProject().getId());
 			statement.setInt(2, entity.getLeader().getId());
 			statement.executeUpdate();
 			resultSet = statement.getGeneratedKeys();
-			if(resultSet.next()) {
+			if (resultSet.next()) {
 				return resultSet.getInt(1);
 			} else {
 				throw new PersistentException();
 			}
-		} catch(SQLException e) {
+		} catch (SQLException e) {
 			throw new PersistentException(e);
 		} finally {
 			try {
 				resultSet.close();
-			} catch(SQLException | NullPointerException e) {}
+			} catch (SQLException | NullPointerException e) {
+			}
 			try {
 				statement.close();
-			} catch(SQLException | NullPointerException e) {}
+			} catch (SQLException | NullPointerException e) {
+			}
 		}
 	}
 
 	@Override
 	public Team read(Integer id) throws PersistentException {
-		Team team = hash.get(id);
-		
-		if(team == null)
-		{		
-			String sql = "SELECT `id`, `project_id`, `leader_id` FROM `team` WHERE `id` = ?";
-			PreparedStatement statement = null;
-			ResultSet resultSet = null;
-			try {
-				statement = this.getConnection().prepareStatement(sql);
-				statement.setInt(1, id);
-				resultSet = statement.executeQuery();
-				//Team team = null;
-				if(resultSet.next()) {
-					team = new Team();
-					team.setId(id);
-					Project project = new Project();
-					project.setId(resultSet.getInt("project_id"));
-					team.setProject(project);
-					User leader = new User();
-					leader.setId(resultSet.getInt("leader_id"));
-					team.setLeader(leader);
-				}				
-				hash.put(id, team);				
-			} catch(SQLException e) {
-				throw new PersistentException(e);
+		if (hash.containsKey(id)) {
+			return hash.get(id);
+		}
+
+		String sql = "SELECT `id`, `project_id`, `leader_id` FROM `team` WHERE `id` = ?";
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			statement = this.getConnection().prepareStatement(sql);
+			statement.setInt(1, id);
+			resultSet = statement.executeQuery();
+			Team team = getEntityFactory( ).create( Team.class );
+			if (resultSet.next()) {
+				team.setId(id);
+				Project project = getEntityFactory().create(Project.class);
+				project.setId(resultSet.getInt("project_id"));
+				team.setProject(project);
+				User leader = getEntityFactory().create(User.class);
+				leader.setId(resultSet.getInt("leader_id"));
+				team.setLeader(leader);
 			}
-			finally {
-				try {
-					resultSet.close();
-				} catch(SQLException | NullPointerException e) {}
-				try {
-					statement.close();
-				} catch(SQLException | NullPointerException e) {}
+			hash.put(id, team);
+			return team;
+		} catch (SQLException | NullPointerException e) {
+			throw new PersistentException(e);
+		} 
+		finally {
+			try {
+				resultSet.close();
+			} catch (SQLException | NullPointerException e) {
+			}
+			try {
+				statement.close();
+			} catch (SQLException | NullPointerException e) {
 			}
 		}
-		return team;
 	}
 
 	@Override
 	public void update(Team entity) throws PersistentException {
-		
+
 		hash.remove(entity.getId());
-		
+
 		String sql = "UPDATE `team` SET `project_id` = ?, `leader_id` = ? WHERE `id` = ?";
 		PreparedStatement statement = null;
 		try {
@@ -98,35 +100,36 @@ public class TeamDaoImpl extends BaseDaoImpl implements TeamDao {
 			statement.setInt(1, entity.getProject().getId());
 			statement.setInt(2, entity.getLeader().getId());
 			statement.setInt(3, entity.getId());
-			int buf = statement.executeUpdate();
-			buf++;
-		} catch(SQLException e) {
+			statement.executeUpdate();
+		} catch (SQLException e) {
 			throw new PersistentException(e);
 		} finally {
 			try {
 				statement.close();
-			} catch(SQLException | NullPointerException e) {}
+			} catch (SQLException | NullPointerException e) {
+			}
 		}
 
 	}
 
 	@Override
 	public void delete(Integer id) throws PersistentException {
-		
+
 		hash.remove(id);
-		
+
 		String sql = "DELETE FROM `team` WHERE `id` = ?";
 		PreparedStatement statement = null;
 		try {
 			statement = getConnection().prepareStatement(sql);
 			statement.setInt(1, id);
 			statement.executeUpdate();
-		} catch(SQLException e) {
+		} catch (SQLException e) {
 			throw new PersistentException(e);
 		} finally {
 			try {
 				statement.close();
-			} catch(SQLException | NullPointerException e) {}
+			} catch (SQLException | NullPointerException e) {
+			}
 		}
 
 	}
--- src/by/vsu/mf/ammc/pm/test/project/management/TeamDaoTest.java
+++ src/by/vsu/mf/ammc/pm/test/project/management/TeamDaoTest.java
@@ -3,6 +3,13 @@ package by.vsu.mf.ammc.pm.test.project.management;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+import by.vsu.mf.ammc.pm.dao.mysql.EntityFactory;
 import by.vsu.mf.ammc.pm.dao.mysql.project.management.TeamDaoImpl;
 import by.vsu.mf.ammc.pm.dao.util.pool.ConnectionPool;
 import by.vsu.mf.ammc.pm.domain.project.Project;
@@ -11,10 +18,19 @@ import by.vsu.mf.ammc.pm.domain.user.User;
 import by.vsu.mf.ammc.pm.exception.PersistentException;
 
 public class TeamDaoTest {
-	public static void main(String[] args) throws PersistentException, SQLException {
+	public static void main(String[] args) throws PersistentException,
+			SQLException {
+		Logger root = Logger.getRootLogger();
+		Layout layout = new PatternLayout("%n%d%n%p\t%C.%M:%L%n%m%n");
+		root.addAppender(new ConsoleAppender(layout));
+		root.setLevel(Level.ALL);
+
 		ConnectionPool pool = ConnectionPool.getInstance();
-		pool.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost", "root", "root", 1, 1, 10000);
+		pool.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost", "root",
+				"root", 1, 1, 10000);
+
 		TeamDaoImpl dao = new TeamDaoImpl();
+		dao.setEntityFactory(new EntityFactory());
 		Connection connection = pool.getConnection();
 		connection.createStatement().execute("use pm_db");
 		dao.setConnection(connection);
@@ -33,7 +49,9 @@ public class TeamDaoTest {
 
 		// read
 		Team read_team = dao.read(create_id);
-		assert create_team.getLeader().getId() == read_team.getLeader().getId() && create_team.getProject().getId() == read_team.getProject().getId() : "Failed on read";
+		assert create_team.getLeader().getId() == read_team.getLeader().getId()
+				&& create_team.getProject().getId() == read_team.getProject()
+						.getId() : "Failed on read";
 
 		// update
 		Team update_team = new Team();
@@ -44,7 +62,9 @@ public class TeamDaoTest {
 		update_team.setProject(project);
 		dao.update(update_team);
 		read_team = dao.read(create_id);
-		assert update_team.getLeader().getId() == read_team.getLeader().getId() && update_team.getProject().getId() == read_team.getProject().getId() : "Failed on update";
+		assert update_team.getLeader().getId() == read_team.getLeader().getId()
+				&& update_team.getProject().getId() == read_team.getProject()
+						.getId() : "Failed on update";
 
 		// delete
 		dao.delete(create_id);
