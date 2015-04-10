bc7496792716cf05fc375230253c48ef593dc63f
mikikusha@gmail.com <mikikusha@gmail.com@7040c96a-f151-7552-e6c8-440674ab3d09>
2015-04-10 10:41:45 +0000

    Исправлены проблемы с ссылками на не существующие id. (Делал Маркевич ,с ноута Ксении)

--- trunk/sql/12_fill_tables.sql
+++ trunk/sql/12_fill_tables.sql
@@ -1,4 +1,3 @@
-12000 to 12999
 /* users_group
    12101-12110
 */
@@ -31,15 +30,15 @@ VALUES
 
 INSERT INTO contact (id,name,user_id,type_id)
 VALUES
-  ( 12121, '111-11-11', 12000, 12111 ),
+  ( 12121, '111-11-11', 12003, 12111 ),
   ( 12122, '222-22-22', 12001, 12111 ),
   ( 12123, '333-33-33', 12002, 12111 ),
 /* eMail */
-  ( 12126, 'users1@mail.ru', 7000, 12112 ),
+  ( 12126, 'users1@mail.ru', 12003, 12112 ),
   ( 12127, 'users2@gmail.com', 12001, 12112 ),
   ( 12128, 'users3@yandex.ru', 12002, 12112 ),
 /* address */
-  ( 12131, 'ул.Лазо 1', 12000, 12113 ),
+  ( 12131, 'ул.Лазо 1', 12002, 12113 ),
   ( 12132, 'ул.Ленина 2', 12001, 12113 ),
   ( 12134, 'ул.Фрунзе 3', 12003, 12113 );
 
@@ -57,7 +56,7 @@ VALUES
 */
 INSERT INTO project (id,name,description,category_id,manager_id)
 VALUES
-  ( 12211, 'MucicS', 'Music Search', 12205, 12001 ),
+  ( 12211, 'MucicS', 'Music Search', 12201, 12001 ),
   ( 12212, 'Steam', 'Steam Game Shop', 12203, 12001 );
 
 /* team
@@ -66,7 +65,7 @@ VALUES
 INSERT INTO team (id,project_id,leader_id)
 VALUES
   ( 12221, 12211, 12002 ),
-  ( 12222, 12212, 12004 );
+  ( 12222, 12212, 12003 );
 
 /* employee
     12231-12240
@@ -80,8 +79,8 @@ VALUES
 
 /*second team*/
   ( 12234, 12003, 12222, 0 ),
-  ( 12235, 12004, 12222, 0 ),
-  ( 12236, 12005, 12222, 1 );
+  ( 12235, 12002, 12222, 0 ),
+  ( 12236, 12001, 12222, 1 );
 
 /* module
    12241-12250
@@ -123,12 +122,12 @@ INSERT INTO tasks_category (id,name,parent_id)
 VALUES
   ( 12291, 'Важная', NULL ),
   ( 12292, 'Обычная', NULL ),
-  ( 6293, 'Не важная', NULL );
+  ( 12293, 'Не важная', NULL );
 
 /* task
    12300-12400
 */
 INSERT INTO task (id,name,description,plan_time,difficulty,open_date,accept_date,close_date,category_id,requirement_id,module_id,employee_id,status)
 VALUES
-  ( 6300, 'front end', 'Создание оформления', 168, 1, 2015-03-20, NULL , NULL , 12291 , 12281 , 12241, NULL , 0),
-  ( 6301, 'back end', 'Создание БД музыки и их классификация', 1, 168, 2015-03-20, NULL , NULL , 12292 , 12281 , 12242, NULL , 0);
+  (12002, "task2", "desc2", 12, 0.2, "2014-11-12", "2014-11-13", null,12291, 12281, null,  12231, 1),
+(12003, "task3", "desc3", 13, 0.3, "2014-11-13", "2014-11-14", "2014-11-15", 12292, null,  12241, 12232, 5);
