80bcf972235be7d549a33c7dcf3ef0341d9dbba0
DIM130394 <DIM130394@gmail.com>
2015-04-11 22:34:33 +0000

    DAO class for issue 29

---
+++ src/by/vsu/mf/ammc/pm/dao/mysql/user/UserDaoImpl.java
@@ -0,0 +1,123 @@
+package by.vsu.mf.ammc.pm.dao.mysql.user;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import by.vsu.mf.ammc.pm.dao.abstraction.user.UserDao;
+import by.vsu.mf.ammc.pm.dao.mysql.BaseDaoImpl;
+import by.vsu.mf.ammc.pm.domain.user.User;
+import by.vsu.mf.ammc.pm.domain.user.UsersGroup;
+import by.vsu.mf.ammc.pm.exception.PersistentException;
+
+public class UserDaoImpl extends BaseDaoImpl implements UserDao{
+	
+	public Integer create(User user) throws PersistentException{
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try{
+			String sqlString = "INSERT INTO user (name, password, first_name, middle_name, last_name, is_admin, group_id) VALUES (?,?,?,?,?,?,?)";
+			statement = getConnection().prepareStatement(sqlString, Statement.RETURN_GENERATED_KEYS);
+			statement.setString(1, user.getName());
+			statement.setString(2, user.getPassword());
+			statement.setString(3, user.getFirstName());
+			statement.setString(4, user.getMiddleName());
+			statement.setString(5, user.getLastName());
+			statement.setBoolean(6, user.getAdmin());
+			statement.setInt(7, user.getGroup().getId());
+			statement.executeUpdate();
+			resultSet = statement.getGeneratedKeys();
+			Integer id = null;
+			if(resultSet.next()){
+				id = resultSet.getInt(1);
+			} else {
+				throw new PersistentException();
+			}
+			return id;
+		} catch(SQLException e){
+			throw new PersistentException(e);
+		} finally {
+			try{
+				resultSet.close();
+			} catch(SQLException | NullPointerException e) {}
+			try {
+				statement.close();
+			} catch(SQLException | NullPointerException e) {}
+		}
+	}
+	
+	public User read(Integer id) throws PersistentException {
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			String sqlString = "Select name, password, first_name, middle_name, last_name, is_admin, group_id from user wwhere id = ?";
+			statement = getConnection().prepareStatement(sqlString, Statement.RETURN_GENERATED_KEYS);
+			statement.setInt(1, id);
+			resultSet = statement.executeQuery();
+			User user = null;
+			if(resultSet.next()){
+				user = new User();
+				user.setId(id);
+				user.setName(resultSet.getString("name"));
+				user.setPassword(resultSet.getString("password"));
+				user.setFirstName(resultSet.getString("first_name"));
+				user.setMiddleName(resultSet.getString("middle_name"));
+				user.setLastName(resultSet.getString("last_name"));
+				user.setAdmin(resultSet.getBoolean("is_admin"));
+				UsersGroup group = new UsersGroup();
+				group.setId(resultSet.getInt("group_id"));
+				user.setGroup(group);
+			}
+			return user;
+		} catch (SQLException e){
+			throw new PersistentException(e);
+		} finally{
+			try {
+				resultSet.close();
+			} catch(SQLException | NullPointerException e) {}
+			try {
+				statement.close();
+			} catch(SQLException | NullPointerException e) {}
+		}
+	}
+	
+	public void update(User user) throws PersistentException {
+		PreparedStatement statement = null;
+		
+		try{
+			String sqlString = "update user set name = ?, password = ?, first_name = ?, middle_name = ?, last_name = ?, is_admin = ?, group_id = ?";
+			statement = getConnection().prepareStatement(sqlString, Statement.RETURN_GENERATED_KEYS);
+			statement.setString(1, user.getName());
+			statement.setString(2, user.getPassword());
+			statement.setString(3, user.getFirstName());
+			statement.setString(4, user.getMiddleName());
+			statement.setString(5, user.getLastName());
+			statement.setBoolean(6, user.getAdmin());
+			statement.setInt(7, user.getGroup().getId());
+			statement.executeUpdate();
+		} catch(SQLException e){
+			throw new PersistentException();
+		} finally {
+			try{
+				statement.close();
+			} catch(SQLException | NullPointerException e){}
+		}
+	}
+	
+	public void delete(Integer id) throws PersistentException {
+		PreparedStatement statement = null;
+		try {
+			String sqString = "delete from user where id = ?";
+			statement = getConnection().prepareStatement(sqString,Statement.RETURN_GENERATED_KEYS);
+			statement.setInt(1,id);
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			throw new PersistentException();
+		} finally{
+			try{
+				statement.close();
+			} catch(SQLException | NullPointerException e){}			
+		}
+	}
+}
