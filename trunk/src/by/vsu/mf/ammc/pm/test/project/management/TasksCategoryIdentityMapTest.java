90d3e04df721682e8a7b28894ce9bebdd88d77bd
yermochenko@gmail.com <yermochenko@gmail.com@7040c96a-f151-7552-e6c8-440674ab3d09>
2015-04-12 21:24:49 +0000

    Change implementation and testing of Identity Map. Change setting of JDBC connection for DAO and Identity Map testing. Fix some other small problems.

--- trunk/src/by/vsu/mf/ammc/pm/test/project/management/TasksCategoryIdentityMapTest.java
+++ trunk/src/by/vsu/mf/ammc/pm/test/project/management/TasksCategoryIdentityMapTest.java
@@ -1,71 +1,104 @@
 package by.vsu.mf.ammc.pm.test.project.management;
 
-import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.FileAppender;
 import org.apache.log4j.Layout;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 
+import by.vsu.mf.ammc.pm.dao.mysql.EntityFactory;
 import by.vsu.mf.ammc.pm.dao.mysql.project.management.TasksCategoryDaoImpl;
 import by.vsu.mf.ammc.pm.dao.util.pool.ConnectionPool;
 import by.vsu.mf.ammc.pm.domain.project.management.TasksCategory;
 import by.vsu.mf.ammc.pm.exception.PersistentException;
 
 public class TasksCategoryIdentityMapTest {
-	public static final String LOG_FILE_NAME = "log.txt";
-	public static final Level LOG_LEVEL = Level.ALL;
-	public static final String LOG_MESSAGE_FORMAT = "%n%d%n%p\t%C.%M:%L%n%m%n";
+	private static TasksCategory read(int id, Connection con) throws PersistentException {
+		TasksCategory category = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String sql = "SELECT `name`, `parent_id` FROM `tasks_category` WHERE `id` = ?";
+		try {
+			preparedStatement = con.prepareStatement(sql);
+			preparedStatement.setInt(1, id);
+			resultSet = preparedStatement.executeQuery();
+			if(resultSet.next()) {
+				category = new TasksCategory();
+				category.setId(id);
+				category.setName(resultSet.getString("name"));
+				Integer parentId = resultSet.getInt("parent_id");
+				if(!resultSet.wasNull()) {
+					TasksCategory parent = new TasksCategory();
+					parent.setId(parentId);
+					category.setParent(parent);
+				}
+			}
+		} catch(SQLException e) {
+			throw new PersistentException(e);
+		} finally {
+			try {
+				resultSet.close();
+			} catch(SQLException | NullPointerException e) {}
+			try {
+				preparedStatement.close();
+			} catch(SQLException | NullPointerException e) {}
+		}
+		return category;
+	}
 
-	public static void main(String[] args) throws PersistentException, SQLException, IOException {
+	public static void main(String[] args) throws PersistentException {
 		Logger root = Logger.getRootLogger();
-		Layout layout = new PatternLayout(LOG_MESSAGE_FORMAT);
-		root.addAppender(new FileAppender(layout, LOG_FILE_NAME, true));
+		Layout layout = new PatternLayout("%n%d%n%p\t%C.%M:%L%n%m%n");
 		root.addAppender(new ConsoleAppender(layout));
-		root.setLevel(LOG_LEVEL);
+		root.setLevel(Level.ALL);
 		ConnectionPool pool = ConnectionPool.getInstance();
-		pool.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost", "root", "root", 1, 1, 10000);
+		pool.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/pm_db", "pm_user", "pm_password", 1, 1, 0);
 		Connection conn = pool.getConnection();
-		conn.createStatement().execute("use pm_db");
 
 		TasksCategoryDaoImpl dao = new TasksCategoryDaoImpl();
 		dao.setConnection(conn);
-		
-		try {
-            List<TasksCategory> list = new ArrayList<TasksCategory>();
-            //create
-            for(int i=0; i<3; i++){
-                TasksCategory tc = new TasksCategory();
-                tc.setName("tc" + (i+1));
-                System.out.println("create tc id = " + dao.create(tc));
-                list.add(tc);
-            }
+		dao.setEntityFactory(new EntityFactory());
 
-            //read
-            for(int i=0; i<3; i++){
-                TasksCategory tc = dao.read(list.get(i).getId());
-                System.out.println("read: id = " + tc.getId() + ", name = " + tc.getName());
-            }
-            
-            //update
-            for(int i=0; i<3; i++){
-                list.get(i).setName("new_tc" + (i+1));
-                dao.update(list.get(i));
-            }
+		// create
+		TasksCategory category = new TasksCategory();
+		category.setName("Test parent category");
+		category.setId(dao.create(category));
+		List<Integer> list = new ArrayList<>();
+		for(int i = 0; i < 1000; i++) {
+			TasksCategory tc = new TasksCategory();
+			tc.setName("tc" + (i + 1));
+			tc.setParent(category);
+			list.add(dao.create(tc));
+		}
 
-            //read
-            for(int i=0; i<3; i++){
-                TasksCategory tc = dao.read(list.get(i).getId());
-                System.out.println("read: id = " + tc.getId() + ", name = " + tc.getName());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		// read without identity map
+		long start1, finish1;
+		start1 = System.currentTimeMillis();
+		for(Integer id : list) {
+			TasksCategory tc = read(id, conn);
+			read(tc.getParent().getId(), conn);
+			//System.out.println("read: id = " + tc.getId() + ", name = " + tc.getName() + ", parent = " + read(tc.getParent().getId(), conn).getName());
+		}
+		finish1 = System.currentTimeMillis();
+
+		// read with identity map
+		long start2, finish2;
+		start2 = System.currentTimeMillis();
+		for(Integer id : list) {
+			TasksCategory tc = dao.read(id);
+			dao.read(tc.getParent().getId());
+			//System.out.println("read: id = " + tc.getId() + ", name = " + tc.getName() + ", parent = " + dao.read(tc.getParent().getId()).getName());
+		}
+		finish2 = System.currentTimeMillis();
+
+		System.out.println("Reading without identity map " + (finish1 - start1) + " ms");
+		System.out.println("Reading with identity map " + (finish2 - start2) + " ms");
 	}
 }
