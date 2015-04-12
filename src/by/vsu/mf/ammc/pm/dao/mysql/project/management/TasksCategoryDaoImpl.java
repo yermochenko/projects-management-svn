58355acda8faccfc2b8acc2ec9f6d91343d734a3
yermochenko <yermochenko@gmail.com>
2015-04-12 21:24:49 +0000

    Change implementation and testing of Identity Map. Change setting of JDBC connection for DAO and Identity Map testing. Fix some other small problems.

--- src/by/vsu/mf/ammc/pm/dao/mysql/project/management/TasksCategoryDaoImpl.java
+++ src/by/vsu/mf/ammc/pm/dao/mysql/project/management/TasksCategoryDaoImpl.java
@@ -1,11 +1,5 @@
 package by.vsu.mf.ammc.pm.dao.mysql.project.management;
 
-import by.vsu.mf.ammc.pm.dao.abstraction.project.management.TasksCategoryDao;
-import by.vsu.mf.ammc.pm.dao.mysql.BaseDaoImpl;
-import by.vsu.mf.ammc.pm.domain.project.management.Task;
-import by.vsu.mf.ammc.pm.domain.project.management.TasksCategory;
-import by.vsu.mf.ammc.pm.exception.PersistentException;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,125 +8,125 @@ import java.sql.Types;
 import java.util.HashMap;
 import java.util.Map;
 
+import by.vsu.mf.ammc.pm.dao.abstraction.project.management.TasksCategoryDao;
+import by.vsu.mf.ammc.pm.dao.mysql.BaseDaoImpl;
+import by.vsu.mf.ammc.pm.domain.project.management.TasksCategory;
+import by.vsu.mf.ammc.pm.exception.PersistentException;
+
 public class TasksCategoryDaoImpl extends BaseDaoImpl implements TasksCategoryDao {
-    private Map<Integer, TasksCategory> map = new HashMap<Integer, TasksCategory>();
+	private Map<Integer, TasksCategory> identityMap = new HashMap<Integer, TasksCategory>();
 
-    @Override
-    public Integer create(TasksCategory entity) throws PersistentException {
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            String sql = "INSERT INTO `tasks_category` (`name`, `parent_id`) VALUES (?, ?)";
-            ps = getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, entity.getName());
-            TasksCategory parent = entity.getParent();
-            if(parent != null && parent.getId() != null) {
-                ps.setInt(2, parent.getId());
-            } else {
-                ps.setNull(2, Types.INTEGER);
-            }
-            ps.executeUpdate();
-            rs = ps.getGeneratedKeys();
-            if(rs.next()) {
-                int id = rs.getInt(1);
-                entity.setId(id);
-                map.put(id, entity);
-                return id;
-            } else {
-                // TODO logger.error
-                throw new PersistentException();
-            }
-        } catch(SQLException e) {
-            throw new PersistentException(e);
-        } finally {
-            try {
-                rs.close();
-            } catch(SQLException | NullPointerException e) {}
-            try {
-                ps.close();
-            } catch(SQLException | NullPointerException e) {}
-        }
-    }
+	@Override
+	public Integer create(TasksCategory category) throws PersistentException {
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String sql = "INSERT INTO `tasks_category` (`name`, `parent_id`) VALUES (?, ?)";
+		try {
+			preparedStatement = getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			preparedStatement.setString(1, category.getName());
+			TasksCategory parent = category.getParent();
+			if(parent != null && parent.getId() != null) {
+				preparedStatement.setInt(2, parent.getId());
+			} else {
+				preparedStatement.setNull(2, Types.INTEGER);
+			}
+			preparedStatement.executeUpdate();
+			resultSet = preparedStatement.getGeneratedKeys();
+			if(resultSet.next()) {
+				return resultSet.getInt(1);
+			} else {
+				throw new PersistentException();
+			}
+		} catch(SQLException e) {
+			throw new PersistentException(e);
+		} finally {
+			try {
+				resultSet.close();
+			} catch(SQLException | NullPointerException e) {}
+			try {
+				preparedStatement.close();
+			} catch(SQLException | NullPointerException e) {}
+			identityMap.clear();
+		}
+	}
 
-    @Override
-    public TasksCategory read(Integer id) throws PersistentException {
-        TasksCategory tc = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        if((tc = map.get(id)) != null) {
-            return tc;
-        }
-        try {
-            String sql = "SELECT `name`, `parent_id` FROM `tasks_category` WHERE `id` = ?";
-            ps = getConnection().prepareStatement(sql);
-            ps.setInt(1, id);
-            rs = ps.executeQuery();
-            if(rs.next()) {
-                tc = getEntityFactory().create(TasksCategory.class);;
-                tc.setId(id);
-                tc.setName(rs.getString("Name"));
-                Integer parentId = rs.getInt("parent_id");
-                if(!rs.wasNull() && parentId != id) {
-                    TasksCategory parent = read(parentId);
-                    tc.setParent(parent);
-                }
-            }
-            if(tc != null){
-                map.put(id, tc);
-            }
-            return tc;
-        } catch(SQLException e) {
-            throw new PersistentException(e);
-        } finally {
-            try {
-                rs.close();
-            } catch(SQLException | NullPointerException e) {}
-            try {
-                ps.close();
-            } catch(SQLException | NullPointerException e) {}
-        }
-    }
+	@Override
+	public TasksCategory read(Integer id) throws PersistentException {
+		TasksCategory category = identityMap.get(id);
+		if(category == null) {
+			PreparedStatement preparedStatement = null;
+			ResultSet resultSet = null;
+			String sql = "SELECT `name`, `parent_id` FROM `tasks_category` WHERE `id` = ?";
+			try {
+				preparedStatement = getConnection().prepareStatement(sql);
+				preparedStatement.setInt(1, id);
+				resultSet = preparedStatement.executeQuery();
+				if(resultSet.next()) {
+					category = getEntityFactory().create(TasksCategory.class);
+					category.setId(id);
+					category.setName(resultSet.getString("name"));
+					Integer parentId = resultSet.getInt("parent_id");
+					if(!resultSet.wasNull()) {
+						TasksCategory parent = getEntityFactory().create(TasksCategory.class);
+						parent.setId(parentId);
+						category.setParent(parent);
+					}
+					identityMap.put(id, category);
+				}
+			} catch(SQLException e) {
+				throw new PersistentException(e);
+			} finally {
+				try {
+					resultSet.close();
+				} catch(SQLException | NullPointerException e) {}
+				try {
+					preparedStatement.close();
+				} catch(SQLException | NullPointerException e) {}
+			}
+		}
+		return category;
+	}
 
-    @Override
-    public void update(TasksCategory entity) throws PersistentException {
-        PreparedStatement ps = null;
-        try {
-            String sql = "UPDATE `tasks_category` SET `name` = ?, `parent_id` = ? WHERE `id` = ?";
-            ps = getConnection().prepareStatement(sql);
-            ps.setString(1, entity.getName());
-            TasksCategory parent = entity.getParent();
-            if(parent != null && parent.getId() != null) {
-                ps.setInt(2, parent.getId());
-            } else {
-                ps.setNull(2, Types.INTEGER);
-            }
-            ps.setInt(3, entity.getId());
-            ps.executeUpdate();
-            map.put(entity.getId(), entity);
-        } catch(SQLException e) {
-            throw new PersistentException(e);
-        } finally {
-            try {
-                ps.close();
-            } catch(SQLException | NullPointerException e) {}
-        }
-    }
+	@Override
+	public void update(TasksCategory category) throws PersistentException {
+		PreparedStatement preparedStatement = null;
+		String sql = "UPDATE `tasks_category` SET `name` = ?, `parent_id` = ? WHERE `id` = ?";
+		try {
+			preparedStatement = getConnection().prepareStatement(sql);
+			preparedStatement.setString(1, category.getName());
+			TasksCategory parent = category.getParent();
+			if(parent != null && parent.getId() != null) {
+				preparedStatement.setInt(2, parent.getId());
+			} else {
+				preparedStatement.setNull(2, Types.INTEGER);
+			}
+			preparedStatement.setInt(3, category.getId());
+			preparedStatement.executeUpdate();
+		} catch(SQLException e) {
+			throw new PersistentException(e);
+		} finally {
+			try {
+				preparedStatement.close();
+			} catch(SQLException | NullPointerException e) {}
+			identityMap.clear();
+		}
+	}
 
-    @Override
-    public void delete(Integer id) throws PersistentException {
-        PreparedStatement ps = null;
-        try {
-            String sql = "DELETE FROM `tasks_category` WHERE `id` = ?";
-            ps = getConnection().prepareStatement(sql);
-            ps.setInt(1, id);
-            ps.executeUpdate();
-            map.remove(id);
-        } catch(SQLException e) {
-            throw new PersistentException(e);
-        } finally {
-            try {
-                ps.close();
-            } catch(SQLException | NullPointerException e) {}
-        }
-    }
+	@Override
+	public void delete(Integer id) throws PersistentException {
+		PreparedStatement preparedStatement = null;
+		String sql = "DELETE FROM `tasks_category` WHERE `id` = ?";
+		try {
+			preparedStatement = getConnection().prepareStatement(sql);
+			preparedStatement.setInt(1, id);
+			preparedStatement.executeUpdate();
+		} catch(SQLException e) {
+			throw new PersistentException(e);
+		} finally {
+			try {
+				preparedStatement.close();
+			} catch(SQLException | NullPointerException e) {}
+			identityMap.clear();
+		}
+	}
 }
--- src/by/vsu/mf/ammc/pm/test/project/management/TasksCategoryDaoTest.java
+++ src/by/vsu/mf/ammc/pm/test/project/management/TasksCategoryDaoTest.java
@@ -1,11 +1,8 @@
 package by.vsu.mf.ammc.pm.test.project.management;
 
-import java.io.IOException;
 import java.sql.Connection;
-import java.sql.SQLException;
 
 import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.FileAppender;
 import org.apache.log4j.Layout;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -18,90 +15,46 @@ import by.vsu.mf.ammc.pm.domain.project.management.TasksCategory;
 import by.vsu.mf.ammc.pm.exception.PersistentException;
 
 public class TasksCategoryDaoTest {
-
-	public static final String LOG_FILE_NAME = "log.txt";
-	public static final Level LOG_LEVEL = Level.ALL;
-	public static final String LOG_MESSAGE_FORMAT = "%n%d%n%p\t%C.%M:%L%n%m%n";
-	
-	public static void main(String[] args) throws IOException, PersistentException, SQLException {
+	public static void main(String[] args) throws PersistentException {
 		Logger root = Logger.getRootLogger();
-		Layout layout = new PatternLayout(LOG_MESSAGE_FORMAT);
-		root.addAppender(new FileAppender(layout, LOG_FILE_NAME, true));
+		Layout layout = new PatternLayout("%n%d%n%p\t%C.%M:%L%n%m%n");
 		root.addAppender(new ConsoleAppender(layout));
-		root.setLevel(LOG_LEVEL);
+		root.setLevel(Level.ALL);
 		ConnectionPool pool = ConnectionPool.getInstance();
-		pool.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost", "root", "root", 1, 1, 10000);
+		pool.init("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/pm_db", "pm_user", "pm_password", 1, 1, 0);
 		Connection connection = pool.getConnection();
-		connection.createStatement().execute("use pm_db");
-		
+
 		TasksCategoryDaoImpl dao = new TasksCategoryDaoImpl();
 		dao.setConnection(connection);
 		dao.setEntityFactory(new EntityFactory());
-		
-		int id1 = 20001;
-		TasksCategory tc1 = null;
-		try {
-			tc1 = dao.getEntityFactory().create(TasksCategory.class);
-			tc1.setName("QQQQQ");
-			id1 = dao.create(tc1);
-			System.out.println("create tc1 id = " + id1);
-		} catch (Exception e) {
-			System.err.println("create tc1");
-			e.printStackTrace();
-		}
-		
-		int id2 = 20002;
-		TasksCategory tc2;
-		try {
-			tc2 = dao.getEntityFactory().create(TasksCategory.class);
-			tc2.setName("WWWWW");
-			tc2.setParent(tc1);
-			id2 = dao.create(tc2);
-			System.out.println("create tc2 id = " + id2);
-		} catch (Exception e) {
-			System.err.println("create tc2");
-			e.printStackTrace();
-		}
-		
-		tc2 = null;
-		try {
-			tc2 = dao.read(id2);
-			System.out.println("read tc2: name = " + tc2.getName());
-		} catch (Exception e) {
-			System.err.println("read tc2");
-			e.printStackTrace();
-		}
-		
-		try {
-			if (tc2 != null) {
-				tc2.setName("EEEEE");
-				dao.update(tc2);
-				System.out.println("update tc2: name = " + dao.read(id2).getName());
-			}
-		} catch(Exception e) {
-			System.err.println("update tc2");
-			e.printStackTrace();
-		}
-		
-		try {
-			if (tc2 != null) {
-				dao.delete(id2);
-				System.out.println("delete tc2: done");
-			}
-		} catch(Exception e) {
-			System.err.println("delete tc2");
-			e.printStackTrace();
-		}
-		
-		try {
-			if (tc1 != null) {
-				dao.delete(id1);
-				System.out.println("delete tc1: done");
-			}
-		} catch(Exception e) {
-			System.err.println("delete tc1");
-			e.printStackTrace();
-		}
-	}
 
+		TasksCategory category1 = null;
+		TasksCategory category2 = null;
+		int id;
+		category1 = new TasksCategory();
+		category1.setName("QQQQQ");
+		id = dao.create(category1);
+		category1.setId(id);
+		System.out.println("create tc1 id = " + id);
+
+		category2 = new TasksCategory();
+		category2.setName("WWWWW");
+		category2.setParent(category1);
+		id = dao.create(category2);
+		category2.setId(id);
+		System.out.println("create tc2 id = " + id);
+
+		category2 = dao.read(id);
+		System.out.println("read tc2: name = " + category2.getName());
+
+		category2.setName("EEEEE");
+		dao.update(category2);
+		System.out.println("update tc2: name = " + dao.read(id).getName());
+
+		dao.delete(category2.getId());
+		System.out.println("delete tc2: done");
+
+		dao.delete(category1.getId());
+		System.out.println("delete tc1: done");
+	}
 }
