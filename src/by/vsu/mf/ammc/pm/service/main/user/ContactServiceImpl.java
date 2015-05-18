b5d7e3444113a77b8ed400444679545c0626340f
prostogerax <prostogerax@gmail.com>
2015-05-18 19:16:49 +0000

    Update issue 83. Status: Done.

--- src/by/vsu/mf/ammc/pm/service/main/user/ContactServiceImpl.java
+++ src/by/vsu/mf/ammc/pm/service/main/user/ContactServiceImpl.java
@@ -1,5 +1,6 @@
 package by.vsu.mf.ammc.pm.service.main.user;
 
+import by.vsu.mf.ammc.pm.dao.abstraction.user.ContactDao;
 import by.vsu.mf.ammc.pm.domain.user.Contact;
 import by.vsu.mf.ammc.pm.exception.PersistentException;
 import by.vsu.mf.ammc.pm.service.abstraction.user.ContactService;
@@ -8,7 +9,20 @@ import by.vsu.mf.ammc.pm.service.main.ServiceImpl;
 public class ContactServiceImpl extends ServiceImpl implements ContactService {
 	@Override
 	public void save(Contact contact) throws PersistentException {
-		// TODO Auto-generated method stub
+		ContactDao contactDao = getTransaction().getDao(ContactDao.class);
+		Integer id = contact.getId();
+		if(id != null){
+			Contact dbContact = contactDao.read(id);
+			if(dbContact !=null){
+				dbContact.setId(contact.getId());
+				dbContact.setName(contact.getName());
+				dbContact.setType(contact.getType());
+				dbContact.setUser(contact.getUser());
+				contactDao.update(dbContact);
+			}
+		}else{
+			contact.setId(contactDao.create(contact));
+		}	
 	}
 
 	@Override
