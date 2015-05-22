77b8b3d4c03c0b414a010729ff723999108d95b6
nixumx <nixumx@7040c96a-f151-7552-e6c8-440674ab3d09>
2015-05-22 10:20:15 +0000

    Update issue 68 Status: Done Created a class by.vsu.mf.ammc.pm.test.junit.project.specification.JUnitActorDaoTest for testing all methods of the class by.vsu.mf.ammc.pm.dao.mysql.project.specification.ActorDaoImpl using framework JUnit.

---
+++ src/by/vsu/mf/ammc/pm/test/junit/project/specification/JUnitActorDaoTest.java
@@ -0,0 +1,86 @@
+package by.vsu.mf.ammc.pm.test.junit.project.specification;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import by.vsu.mf.ammc.pm.dao.mysql.EntityFactory;
+import by.vsu.mf.ammc.pm.dao.mysql.project.specification.ActorDaoImpl;
+import by.vsu.mf.ammc.pm.domain.project.Project;
+import by.vsu.mf.ammc.pm.domain.project.specification.Actor;
+import by.vsu.mf.ammc.pm.exception.PersistentException;
+
+public class JUnitActorDaoTest extends Assert {
+	@Test
+	public void testCRUD() {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost", "root", "root");
+		} catch(SQLException e) { 
+			e.printStackTrace();
+		}
+
+		ActorDaoImpl actorDaoImpl = new ActorDaoImpl();
+		actorDaoImpl.setConnection(connection);
+		actorDaoImpl.setEntityFactory(new EntityFactory());
+
+		Actor actor = actorDaoImpl.getEntityFactory().create(Actor.class);
+		Integer actorId=11999;
+		Integer projectId=11999;
+		actor.setId(actorId);
+		actor.setName("testActor");		
+		Project project = new Project();
+		project.setId(projectId);
+		actor.setProject(project);		
+
+		try {
+			connection.prepareStatement("use pm_db").executeQuery();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+		// create		
+		try {
+			actorDaoImpl.create(actor);			
+		} catch(PersistentException e) {
+			Assert.assertTrue(false);
+		}		
+
+		// read
+		Actor actorReaded = null;
+		try {
+			actorReaded = actorDaoImpl.read(actorId);
+		} catch(PersistentException e) {
+			Assert.assertTrue(false);
+		}
+		Assert.assertEquals(actor.getId(), actorReaded.getId());		
+		Assert.assertEquals(actor.getName(), actorReaded.getName());
+		Assert.assertEquals(actor.getProject(), actorReaded.getProject());
+		
+
+		// update
+		try {
+			actor.setName("NewName");
+			actorDaoImpl.update(actor);
+			actorReaded = actorDaoImpl.read(actorId);
+		} catch(PersistentException e) {
+			Assert.assertTrue(false);
+		}
+
+		Assert.assertEquals(actor.getName(), actorReaded.getName());
+
+		// delete
+		try {
+			actorDaoImpl.delete(actorId);
+			actorReaded = actorDaoImpl.read(actorId);
+		} catch(PersistentException e) {
+			Assert.assertTrue(false);
+		}
+
+		Assert.assertNull(actorReaded);
+	}
+
+}
