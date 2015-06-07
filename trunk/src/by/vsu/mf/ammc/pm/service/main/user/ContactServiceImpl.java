19d3922ad921d529be45ae5b9134df505a235124
Ragvarod@gmail.com <Ragvarod@gmail.com@7040c96a-f151-7552-e6c8-440674ab3d09>
2015-06-07 19:18:12 +0000

    Method delete ( ) is made

--- trunk/src/by/vsu/mf/ammc/pm/service/main/user/ContactServiceImpl.java
+++ trunk/src/by/vsu/mf/ammc/pm/service/main/user/ContactServiceImpl.java
@@ -1,6 +1,7 @@
 package by.vsu.mf.ammc.pm.service.main.user;
 
 import by.vsu.mf.ammc.pm.dao.abstraction.user.ContactDao;
+import by.vsu.mf.ammc.pm.dao.abstraction.user.UserDao;
 import by.vsu.mf.ammc.pm.domain.user.Contact;
 import by.vsu.mf.ammc.pm.exception.PersistentException;
 import by.vsu.mf.ammc.pm.service.abstraction.user.ContactService;
@@ -21,5 +22,8 @@ public class ContactServiceImpl extends ServiceImpl implements ContactService {
 	@Override
 	public void delete(int id) throws PersistentException {
 		// TODO Auto-generated method stub
+	
+				this.getTransaction().getDao(ContactDao.class).delete(id);
+			
 	}
 }
