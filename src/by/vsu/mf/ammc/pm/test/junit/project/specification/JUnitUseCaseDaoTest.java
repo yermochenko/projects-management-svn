8d0f6999b7d1c5e86a957ac412648adedacd5435
gvarod <Ragvarod@gmail.com>
2015-06-07 17:14:07 +0000

    JUnit achieved

---
+++ src/by/vsu/mf/ammc/pm/test/junit/project/specification/JUnitUseCaseDaoTest.java
@@ -0,0 +1,85 @@
+package by.vsu.mf.ammc.pm.test.junit.project.specification;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import by.vsu.mf.ammc.pm.dao.mysql.EntityFactory;
+import by.vsu.mf.ammc.pm.dao.mysql.project.specification.UseCaseDaoImpl;
+import by.vsu.mf.ammc.pm.domain.project.Module;
+import by.vsu.mf.ammc.pm.domain.project.specification.UseCase;
+import by.vsu.mf.ammc.pm.exception.PersistentException;
+
+public class JUnitUseCaseDaoTest extends Assert {
+	@Test
+	public void testCRUD() {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost", "root", "root");
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+		UseCaseDaoImpl useCaseDaoImpl = new UseCaseDaoImpl();
+		useCaseDaoImpl.setConnection(connection);
+		useCaseDaoImpl.setEntityFactory(new EntityFactory());
+
+		UseCase useCase = useCaseDaoImpl.getEntityFactory().create(UseCase.class);
+		Integer useCaseId = 11999;
+		Integer moduleId = 11999;
+		useCase.setId(useCaseId);
+		useCase.setName("testActor");
+		Module module = new Module();
+		module.setId(moduleId);
+		useCase.setModule(module);
+
+		try {
+			connection.prepareStatement("use pm_db").executeQuery();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+		// create
+		try {
+			useCaseDaoImpl.create(useCase);
+		} catch(PersistentException e) {
+			Assert.assertTrue(false);
+		}
+
+		// read
+		UseCase UseCaseReaded = null;
+		try {
+			UseCaseReaded = useCaseDaoImpl.read(useCaseId);
+		} catch(PersistentException e) {
+			Assert.assertTrue(false);
+		}
+		Assert.assertEquals(useCase.getId(), UseCaseReaded.getId());
+		Assert.assertEquals(useCase.getName(), UseCaseReaded.getName());
+		Assert.assertEquals(useCase.getModule(), UseCaseReaded.getModule());
+
+		// update
+		try {
+			useCase.setName("NewName");
+			useCaseDaoImpl.update(useCase);
+			UseCaseReaded = useCaseDaoImpl.read(useCaseId);
+		} catch(PersistentException e) {
+			Assert.assertTrue(false);
+		}
+
+		Assert.assertEquals(useCase.getName(), UseCaseReaded.getName());
+
+		// delete
+		try {
+			useCaseDaoImpl.delete(useCaseId);
+			UseCaseReaded = useCaseDaoImpl.read(useCaseId);
+		} catch(PersistentException e) {
+			Assert.assertTrue(false);
+		}
+
+		Assert.assertNull(UseCaseReaded);
+	}
+
+}
