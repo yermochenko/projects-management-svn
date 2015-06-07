6752dfc2545b8475fa4d9b3c907fc27a2cfcfc65
newjeka777@gmail.com <newjeka777@gmail.com@7040c96a-f151-7552-e6c8-440674ab3d09>
2015-06-07 20:15:42 +0000

    performing Issue 81 and modified sql file

--- trunk/src/by/vsu/mf/ammc/pm/dao/abstraction/project/management/EmployeeDao.java
+++ trunk/src/by/vsu/mf/ammc/pm/dao/abstraction/project/management/EmployeeDao.java
@@ -1,14 +1,20 @@
 package by.vsu.mf.ammc.pm.dao.abstraction.project.management;
 
 import by.vsu.mf.ammc.pm.dao.abstraction.Dao;
+import by.vsu.mf.ammc.pm.domain.project.Module;
 import by.vsu.mf.ammc.pm.domain.project.Project;
 import by.vsu.mf.ammc.pm.domain.project.management.Employee;
 import by.vsu.mf.ammc.pm.domain.project.management.EmployeesRole;
 import by.vsu.mf.ammc.pm.domain.user.User;
+import by.vsu.mf.ammc.pm.exception.PersistentException;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public interface EmployeeDao extends Dao< Employee > {
     Map< Project, List< EmployeesRole > > readByUser( User user );
+    
+    boolean employeeByUserIsEmpty(Integer user_id) throws SQLException;
 }
