47768c0ee64eaec0d840e7aa6ab7c851ed3a8090
vadjatr <vadjatr@gmail.com>
2015-06-17 12:30:48 +0000

    Update issue 102. Status: Done

--- src/by/vsu/mf/ammc/pm/dao/abstraction/project/ProjectsCategoryDao.java
+++ src/by/vsu/mf/ammc/pm/dao/abstraction/project/ProjectsCategoryDao.java
@@ -8,5 +8,7 @@ import by.vsu.mf.ammc.pm.domain.user.ContactsType;
 import by.vsu.mf.ammc.pm.exception.PersistentException;
 
 public interface ProjectsCategoryDao extends Dao<ProjectsCategory> {
-	List<ProjectsCategory> readAll() throws PersistentException;	
+	List<ProjectsCategory> readAll() throws PersistentException;
+
+	List<ProjectsCategory> readChildsByProjectsCategory(ProjectsCategory projectsCategory) throws PersistentException;	
 }
